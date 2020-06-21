22baf8f504ff80297e2da1f357459362cc5883d8
Kishor Ghising <bhfty99@gmail.com>
2020-06-21 13:15:15 +0545

    Test props types for Congrats

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import Congrats from './Congrats';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+const defaultProps = { success: false };
 
 const setup = (props={}) => {
-    return shallow(<Congrats {...props} />);
+    const setupProps = {...defaultProps, ...props};
+    return shallow(<Congrats {...setupProps} />);
 };
 
 test('render without error', () => {
@@ -26,4 +26,8 @@ test('render non empty congrats message when props is true', () => {
     const wrapper = setup({ success: true });
     const message = findByTestAttr(wrapper, 'congrats-message');
     expect(message.text().length).not.toBe(0);
+});
+test('does not throw warning with expected props', () => {
+    const expectedProps = { success: false };
+    checkProps(Congrats, expectedProps);
 });
